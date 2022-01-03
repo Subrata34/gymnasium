de5d0fb5bcf704ead56b3285670adcdba7c9fe77
Subrata34 <subratamodak3491@gmail.com>
2022-01-03 14:03:56 +0600

    gym

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <ul className='nav-bar'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Package</li>
+                <li>Our Team</li>
+                <li>Register</li>
+                <li>Login</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
